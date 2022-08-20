ab9a75a385848f1bb7eb4a70638bfb526046a46e
cpesar <cpesar1@gmail.com>
2022-08-20 15:20:59 -0600

    fetching data with jsonplaceholder api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "react-query";
+import fetchPosts from "./FetchApi";
+import Form from "./Form";
+import "./App.css";
 
 function App() {
+  const { data, error, isLoading, isError } = useQuery("users", fetchPosts);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error! {error.message}</div>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" ">
+      <h1 className="">Users Name</h1>
+
+      {data.map((users, id) => {
+        return (
+          <ul>
+            <li className="" key={id}>
+              {users.name}
+            </li>
+          </ul>
+        );
+      })}
+      <Form />
     </div>
   );
 }
